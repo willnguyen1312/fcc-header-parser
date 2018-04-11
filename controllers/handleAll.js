3019d9132fbb561a75904262aa481bf277a884c4
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-04-11 16:30:17 +0700

    Production ready

--- controllers/handleAll.js
+++ controllers/handleAll.js
@@ -1,48 +1,14 @@
-const moment = require("moment");
-const Joi = require("joi");
+const matchAll = require("string.prototype.matchall");
 
-const regex = /\S+ \d{1,2}, \d{4}/;
-
-const schema = Joi.object().keys({
-  unix: Joi.number(),
-  natural: Joi.string().regex(regex)
-});
 function hello(ctx) {
-  // const day = moment.unix(1450137600);
-
-  // console.log(day.format(`MMMM D, YYYY`));
-
-  // const str = `December 15, 2015`;
-  // console.log(moment(str, `MMMM D, YYYY`).unix() + 25200);
-
-  // console.log(1450112400 - 1450137600);
-  let result = {
-    unix: undefined,
-    natural: undefined
+  const language = ctx.header["accept-language"];
+  const agent = ctx.header["user-agent"];
+  const software = agent.substring(agent.indexOf("(") + 1, agent.indexOf(")"));
+  ctx.body = {
+    ipaddress: ctx.ip,
+    language: language.slice(0, language.indexOf(",")),
+    software
   };
-
-  const query = ctx.params.query;
-
-  if (query.indexOf(" ") === -1) {
-    const natural = moment.unix(query).format(`MMMM D, YYYY`);
-    result = {
-      unix: Number(query),
-      natural
-    };
-  } else {
-    const unixNumber = moment(query, `MMMM D, YYYY`).unix() + 25200;
-    result = {
-      unix: unixNumber,
-      natural: query
-    };
-  }
-  const valid = Joi.validate(result, schema);
-  if (valid.error) {
-    result.unix = null;
-    result.natural = null;
-  }
-
-  ctx.body = result;
 }
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,3 @@
 module.exports = router => {
-  router.get("/", ctx => {
-    ctx.body = "Please give me timestamp as part of the url";
-  });
-  router.use("/:query", require("./handleAll"));
-  router.get("/*", ctx => {
-    ctx.body = "Please give me a valid route";
-  });
+  router.use("/*", require("./handleAll"));
 };
